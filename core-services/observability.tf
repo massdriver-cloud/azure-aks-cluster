8ef601281434f7296ca8ce69b883c8b346686dc3
Jake Ferriero <jake@massdriver.cloud>
2022-08-19 09:25:24 -0700

    add opensearch option

--- core-services/observability.tf
+++ core-services/observability.tf
@@ -13,3 +13,23 @@ module "kube-state-metrics" {
   release     = "kube-state-metrics"
   namespace   = kubernetes_namespace.md-observability.metadata.0.name
 }
+
+module "opensearch" {
+  count       = var.observability.logging.destination == "opensearch" ? 1 : 0
+  # TODO replace ref with a SHA once k8s-opensearch is merged
+  source      = "github.com/massdriver-cloud/terraform-modules//k8s-opensearch?ref=opensearch"
+  md_metadata = var.md_metadata
+  release     = "opensearch"
+  namespace   = "md-observability" # TODO should this be monitoring?
+  kubernetes_cluster =  local.kubernetes_cluster_artifact
+  helm_additional_values = {
+    persistence = {
+        size = var.observability.logging.opensearch.persistence_size
+    }
+  } 
+  enable_dashboards = var.observability.logging.opensearch.enable_dashboards
+  // this adds a retention policy to move indexes to warm after 1 day and delete them after a user configurable number of days
+  ism_policies = {
+    "hot-warm-delete": templatefile("${path.module}/logging/opensearch/ism_hot_warm_delete.json.tftpl", {"log_retention_days": var.observability.logging.opensearch.retention_days})
+  }
+}
