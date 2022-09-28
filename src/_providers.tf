7df2058b071d41cb718b89dd6b845c86400a8edb
mclacore <michael@massdriver.cloud>
2022-09-28 15:45:02 -0600

    CMK deploying with whitelisted IP address

--- src/_providers.tf
+++ src/_providers.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0"
+    }
   }
 }
 
@@ -15,4 +19,10 @@ provider "azurerm" {
   tenant_id       = var.azure_service_principal.data.tenant_id
   client_secret   = var.azure_service_principal.data.client_secret
   subscription_id = var.azure_service_principal.data.subscription_id
+}
+
+provider "azuread" {
+  client_id     = var.azure_service_principal.data.client_id
+  tenant_id     = var.azure_service_principal.data.tenant_id
+  client_secret = var.azure_service_principal.data.client_secret
 }
