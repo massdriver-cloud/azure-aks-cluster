d1e0c7a34293d37c0d40c94bc0473a940ad7bd02
Will Beebe <will@containerlabs.io>
2023-01-06 15:59:57 -0800

    Update src/main.tf

--- src/main.tf
+++ src/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     vm_size             = var.node_groups.default_node_group.node_size
     min_count           = var.node_groups.default_node_group.min_size
     max_count           = var.node_groups.default_node_group.max_size
-    vnet_subnet_id      = var.azure_virtual_network.data.infrastructure.default_subnet_id
+    vnet_subnet_id      = var.vnet.data.infrastructure.default_subnet_id
     enable_auto_scaling = true
     tags                = var.md_metadata.default_tags
   }
